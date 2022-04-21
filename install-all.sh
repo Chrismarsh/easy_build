87785325bfddb4d779bef3265ab26aba585dce5b
Chris Marsh <chrismarsh.c2@gmail.com>
2022-04-21 11:00:59 -0700

    better install all that handles dependency order.

--- install-all.sh
+++ install-all.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-for f in *.eb;
-do
-    eb "$f" --force --inject-checksums
-    eb "$f" --force
-done
+eb Armadillo-10.2.0-gompi-2020a.eb 
+eb func-master-GCC-9.3.0.eb --force
+eb CGAL-5.2-gompi-2020a.eb --force
+
+eb ESMF-8.3.0b10-gomkl-2020a.eb --force
+eb ESMPy-8.3.0b10-gomkl-2020a.eb --force
+
+
+eb SQLite-chm-3.37.0-GCCcore-10.3.0.eb --force
+eb PROJ-chm-7.2.1-GCCcore-10.3.0.eb --force
+eb GDAL-chm-3.2.2-GCC-9.3.0.eb --force
+
+eb gperftools-2.8-GCCcore-9.3.0.eb --force
+eb meteoio-2.8.0-GCC-9.3.0.eb --force
+
+eb sparsehash-2.0.4-GCC-9.3.0.eb --force
+
+eb trilinos-13-chm-GCC-9.3.0.eb --force
